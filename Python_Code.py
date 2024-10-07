1dcfbe73e3cb5ea174732e8c10c44f3e6f4b3acf
Keanu Valencia <104657054+keanudv@users.noreply.github.com>
2024-10-06 20:53:37 -1000

    Update Python_Code.py

--- Python_Code.py
+++ Python_Code.py
@@ -22,7 +22,6 @@ df.tail()
 
 # Select only the needed columns
 columns = df[["City","Sold_Price"]]
-print(columns)
 
 # Create a new dataframe with the selected columns
 new_df = columns
@@ -30,14 +29,14 @@ new_df.head()
 
 # Drop missing values and create the final dataframe for analysis
 final_df = new_df.dropna()
+final_df.isnull().sum()
 
 # Exploratory Data Analysis (EDA)
-final_df.isnull().sum()
 final_df.shape
+final_df.dtypes
 final_df.describe()
 final_df.max()
 final_df.min()
-final_df.dtypes
 
 # Set the null hypothesis and significance level
 null_hypothesis = 1_500_000
@@ -46,24 +45,28 @@ alpha = 0.05
 # Extract the sold prices
 sold_prices = final_df["Sold_Price"]
 
-# Perform the t-test
+# Calculate the Test Statistic, P-Value, and Critical Value
 t_stat, p_value = stats.ttest_1samp(sold_prices, null_hypothesis)
+critical_value = stats.t.ppf(1-alpha/2, len(sold_prices)-1)
 print(f"t-STAT: {t_stat:.4f}")
 print(f"P-Value: {p_value:.4f}")
+print(f"Critical Value: {critical_value:.4f}")
 
-# conclusion
+# Conclusion using the Test-Statistic
+if t_stat > critical_value:
+  print("Reject the null hypothesis. There is sufficient evidence to conclude that the mean sold price of homes in Maui is not $1.5 million.")
+else:
+  print("Do not reject the null hypothesis. There is not sufficient evidence to conclude that the mean sold price of homes in Maui is different from $1.5 million")
+
+# Conclusion using the P-Value
 if p_value < alpha:
   print("Reject the null hypothesis. There is sufficient evidence to conclude that the mean sold price of homes in Maui is not $1.5 million.")
 else:
   print("Do not reject the null hypothesis. There is not sufficient evidence to conclude that the mean sold price of homes in Maui is different from $1.5 million.")
 
-# Calculate the Critical Value for the t-distribution
-degrees_of_freedom = len(sold_prices)-1
-critical_value = stats.t.ppf(1-alpha/2, degrees_of_freedom)
-
 # Create the sampling distribution
 x = np.linspace(-4,4,1000)
-y = stats.t.pdf(x, degrees_of_freedom)
+y = stats.t.pdf(x, len(sold_prices)-1)
 plt.plot(x,y, label="t-dist", color="blue")
 plt.fill_between(x,y, where=(x > critical_value) | (x < -critical_value), color="red", alpha=alpha, label="Rejection Region")
 plt.axvline(t_stat, color="green", linestyle="--", label=f"t-STAT = {t_stat:.4f}")
